f4a7eb82531dc37e402239835387e1af3eb7455c
tomsta93 <thomasstapleton0@gmail.com>
2016-04-23 16:31:34 +0100

    Added functionality to retrieve the flickr endpoint, once applied this change will add browser support for ES5 and the SystemJS lib, images are also returned from the Flickr endpoint.

---
+++ src/javascript/display-images.js
@@ -0,0 +1,6 @@
+export default class DisplayImages {
+  constructor (data) {
+    this.images = data.items;
+    console.log(data.items);
+  }
+}
---
+++ src/javascript/main.js
@@ -0,0 +1,14 @@
+import DisplayImages from './display-images';
+
+const TAGS = 'london'
+const API = `http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=dataHandler&tags=${TAGS}`;
+
+window.dataHandler = function (data) {
+  new DisplayImages(data);
+};
+
+let script = document.createElement('script');
+script.src = API;
+document.body.appendChild(script);
+
+export let callback = 'dataHandler';
