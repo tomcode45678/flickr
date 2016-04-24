5b45e0d0afcce5713c4c2951c0015e2626b7eb5d
tomsta93 <thomasstapleton0@gmail.com>
2016-04-24 20:31:20 +0100

    Added render functionality for images, once applied this change will display all information retrieved from the flickr API.

--- src/javascript/display-images.js
+++ src/javascript/display-images.js
@@ -1,8 +1,25 @@
-/* globals console */
+/* globals document */
 
 export default class DisplayImages {
   constructor (data) {
     this.images = data.items;
-    console.log(data.items);
+    this.imageContainer = document.querySelector('[data-image-container]');
+    this.renderAssets(data.items);
+  }
+
+  renderAssets(assets) {
+    let imageFragments = document.createDocumentFragment();
+
+    for (let i = 0, assetsLength = assets.length; i < assetsLength; i++) {
+      let asset = assets[i];
+      imageFragments.appendChild(this.renderAsset(asset));
+    }
+    this.imageContainer.appendChild(imageFragments);
+  }
+
+  renderAsset(asset) {
+    let article = document.createElement('article');
+    article.innerHTML = asset.description;
+    return article;
   }
 }
