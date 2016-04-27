2bc78b87fa3d3e6514198eb9eaf75188b1ad0d56
tomsta93 <thomasstapleton0@gmail.com>
2016-04-27 22:25:05 +0100

    Added visual loading functionality.
    Once applied this change will inform the user a request is being made meaning DOMContent load is as optimal as possible. Once the data is loaded the spinner is removed.

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -2,9 +2,14 @@ import DisplayImages from './display-images';
 
 const TAGS = 'london';
 const API = `http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=dataHandler&tags=${TAGS}`;
+const loadingSpinner = document.querySelector('[data-loading-spinner]');
 
 window.dataHandler = function (data) {
   new DisplayImages(data);
+  
+  if (loadingSpinner) {
+    loadingSpinner.classList.remove('show');
+  }
 };
 
 let script = document.createElement('script');
